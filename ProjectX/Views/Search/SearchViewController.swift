ccc18b58bb10918bfc70fe33b2c57e1935fb677e
Md. Akhlak <mohammadakhlak121@gmail.com>
2025-01-07 16:49:37 +0530

    added tab tar

---
+++ ProjectX/Views/Search/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  ProjectX
+//
+//  Created by Batch - 1 on 07/01/25.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
