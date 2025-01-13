acf54a2a113e9b9b87e7a4495e1ae6748eb347b8
Md. Akhlak <mohammadakhlak121@gmail.com>
2025-01-13 15:33:37 +0530

    added: Calendar collection view cell

---
+++ LearnSathi/customViews/Schedule/CalendarCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarCollectionViewCell.swift
+//  LearnSathi
+//
+//  Created by Batch - 1 on 13/01/25.
+//
+
+import UIKit
+
+class CalendarCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
