8ce3a1b090f63dfbd7dc4eaf1765da6068fc86fa
Piyush Verma <vermapiyush444444@gmail.com>
2025-01-13 15:27:43 +0530

    Added: Data Model class

---
+++ LearnSathi/Data Model/Progress.swift
@@ -0,0 +1,16 @@
+//
+//  Progress.swift
+//  LearnSathi
+//
+//  Created by Batch - 2 on 13/01/25.
+//
+
+import Foundation
+
+struct Progress {
+    var tutorLogo, tutorName, duration, subjectLogo, subjectName: String
+    var date : Date
+    var startTime, endTime: String
+    var topicDescription: [String]
+    var lessonNumber: Int
+}
