d1522212e3bd3ef282a3825869da36c5d7c8b92c
Piyush Verma <vermapiyush444444@gmail.com>
2025-01-13 16:47:43 +0530

    Added: Progress feature dataModel, XIB files

--- LearnSathi/Data Model/Progress.swift
+++ LearnSathi/Data Model/Progress.swift
@@ -8,9 +8,19 @@
 import Foundation
 
 struct Progress {
-    var tutorLogo, tutorName, duration, subjectLogo, subjectName: String
-    var date : Date
-    var startTime, endTime: String
-    var topicDescription: [String]
-    var lessonNumber: Int
+    var subject: String
+    var lessonOngoing: Int
+    var lessonStatus: String
+    var totalLessons: Int
+    var completedPercentage: Double
+    var subjectLogo: String
+    init (subject: String, lessonOngoing: Int, lessonStatus: String, totalLessons: Int, completedPercentage: Double, subjectLogo: String) {
+        self.subject = subject
+        self.lessonOngoing = lessonOngoing
+        self.lessonStatus = lessonStatus
+        self.totalLessons = totalLessons
+        self.completedPercentage = completedPercentage
+        self.subjectLogo = subjectLogo
+    }
 }
+let progressMaths : [Progress] = [Progress(subject: "Mathematics", lessonOngoing: 12, lessonStatus: "In Progress", totalLessons: 18, completedPercentage: 66.6, subjectLogo: "Maths")]
