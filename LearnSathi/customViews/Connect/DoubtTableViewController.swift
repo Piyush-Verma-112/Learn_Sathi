96821ecea4707b00e9ba2c37a3949ba119576abc
Praveen Shrivastav <praveenshrivastav553@gmail.com>
2025-01-27 16:56:09 +0530

    modified: doubt section in connect hub

--- LearnSathi/customViews/Connect/DoubtTableViewController.swift
+++ LearnSathi/customViews/Connect/DoubtTableViewController.swift
@@ -2,27 +2,30 @@
 //  DoubtTableViewController.swift
 //  LearnSathi
 //
-//  Created by Batch - 2 on 21/01/25.
+//  Created by Batch - 2 on 27/01/25.
 //
 
 import UIKit
+
 protocol DoubtDelegate: AnyObject {
     func didAddDoubt(_ newDoubt: DoubtsListTableViewController.Subject)
 }
+
 class DoubtTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     weak var delegate: DoubtDelegate?
-    var imageStored: [String] = []
+    var selectedImages: [UIImage] = []
     let subjects = ["English", "Hindi", "Mathematics", "Social Science"]
-        var lessonsMapping: [String: [String]] = [
-            "English": chapterDetailsEnglish.map { $0.chapterName },
-            "Hindi": chapterDetailsHindi.map { $0.chapterName },
-            "Mathematics": chapterDetailsMaths.map { $0.chapterName },
-            "Social Science": chapterDetailsSocialStudies.map { $0.chapterName }
-        ]
+    var lessonsMapping: [String: [String]] = [
+        "English": chapterDetailsEnglish.map { $0.chapterName },
+        "Hindi": chapterDetailsHindi.map { $0.chapterName },
+        "Mathematics": chapterDetailsMaths.map { $0.chapterName },
+        "Social Science": chapterDetailsSocialStudies.map { $0.chapterName }
+    ]
     var currentLessons: [String] = []
-    
+
     @IBOutlet weak var subjectLabel: UILabel!
+    
     
     @IBOutlet weak var lessonLabel: UILabel!
     
@@ -30,130 +33,191 @@ class DoubtTableViewController: UITableViewController, UIImagePickerControllerDe
     
     @IBOutlet weak var lessonButton: UIButton!
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var questionTextView: UITextView!
     
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
-            super.viewDidLoad()
-            setupMenus()
-            setupImageView()
-        }
-
-        // MARK: - Setup ImageView Tap Gesture
-        private func setupImageView() {
-            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(uploadButtonTapped(_:)))
-            imageView.isUserInteractionEnabled = true
-            imageView.addGestureRecognizer(tapGesture)
-        }
-
-        // MARK: - Camera Action
-        @IBAction func uploadButtonTapped(_ sender: UIButton) {
-            let imagePickerController = UIImagePickerController()
-            imagePickerController.delegate = self
-
-            let alertController = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-
-            if UIImagePickerController.isSourceTypeAvailable(.camera) {
-                let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
-                    imagePickerController.sourceType = .camera
-                    self.present(imagePickerController, animated: true, completion: nil)
-                }
-                alertController.addAction(cameraAction)
+        super.viewDidLoad()
+        setupMenus()
+        let nib = UINib(nibName: "ImageCollectionViewCell", bundle: nil)
+            collectionView.register(nib, forCellWithReuseIdentifier: "ImageCell")
+        setupCollectionView()
+        
+    }
+    private func setupMenus() {
+        var subjectActions: [UIAction] = []
+        for subject in subjects {
+            let action = UIAction(title: subject) { [weak self] _ in
+                self?.subjectLabel.text = subject
+                self?.updateLessons(for: subject)
             }
+            subjectActions.append(action)
+        }
 
-            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-                let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { _ in
-                    imagePickerController.sourceType = .photoLibrary
-                    self.present(imagePickerController, animated: true, completion: nil)
-                }
-                alertController.addAction(photoLibraryAction)
+        let subjectMenu = UIMenu(title: "Choose Subject", children: subjectActions)
+        subjectButton.menu = subjectMenu // Ensure this line is present
+        subjectButton.showsMenuAsPrimaryAction = true
+        updateLessons(for: subjects.first ?? "")
+    }
+
+    private func updateLessons(for subject: String) {
+        currentLessons = lessonsMapping[subject] ?? []
+        var lessonActions: [UIAction] = []
+        for lesson in currentLessons {
+            let action = UIAction(title: lesson) { [weak self] _ in
+                self?.lessonLabel.text = lesson
             }
+            lessonActions.append(action)
+        }
+        let lessonMenu = UIMenu(title: "Choose Lesson", children: lessonActions)
+        lessonButton.menu = lessonMenu // Ensure this line is present
+        lessonButton.showsMenuAsPrimaryAction = true
+    }
 
-            alertController.addAction(cancelAction)
+    // MARK: - Setup CollectionView
+    private func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
 
-            if let popoverController = alertController.popoverPresentationController {
-                popoverController.sourceView = sender
-                popoverController.sourceRect = sender.bounds
+    // MARK: - Add Image
+    @IBAction func addImageTapped(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+
+        let alertController = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
             }
-
-            present(alertController, animated: true, completion: nil)
+            alertController.addAction(cameraAction)
         }
 
-        // MARK: - UIImagePickerControllerDelegate
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let selectedImage = info[.originalImage] as? UIImage {
-                imageView.image = selectedImage
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { _ in
+                imagePickerController.sourceType = .photoLibrary
+                self.present(imagePickerController, animated: true, completion: nil)
             }
-            dismiss(animated: true, completion: nil)
+            alertController.addAction(photoLibraryAction)
         }
 
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            dismiss(animated: true, completion: nil)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+    // MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            selectedImages.append(selectedImage)
+            collectionView.reloadData()
         }
+        dismiss(animated: true, completion: nil)
+    }
 
-        // MARK: - Setup Menus
-        private func setupMenus() {
-            var subjectActions: [UIAction] = []
-            for subject in subjects {
-                let action = UIAction(title: subject) { [weak self] _ in
-                    self?.subjectLabel.text = subject
-                    self?.updateLessons(for: subject)
-                }
-                subjectActions.append(action)
-            }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 
-            let subjectMenu = UIMenu(title: "Choose Subject", children: subjectActions)
-            subjectButton.menu = subjectMenu
-            subjectButton.showsMenuAsPrimaryAction = true
-            updateLessons(for: subjects.first ?? "")
-        }
+    // MARK: - Done Button Action
+    @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
+        print("Done button tapped")
+        guard let subjectText = subjectLabel.text, !subjectText.isEmpty,
+                  let lessonText = lessonLabel.text, !lessonText.isEmpty,
+                  let questionText = questionTextView.text, !questionText.isEmpty else {
+                
+                // If any of them are missing, show an alert
+                let alertController = UIAlertController(
+                    title: "Incomplete Information",
+                    message: "Please select a subject, lesson, and type or scan a question.",
+                    preferredStyle: .alert
+                )
+               alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+               present(alertController, animated: true, completion: nil)
+               return
+           }
+           
+           let newDoubt = DoubtsListTableViewController.Subject(
+               image: "defaultImage", // Provide a default or selected image
+               subjectName: subjectText,
+               lessonName: lessonText,
+               status: "Pending", // Set default status
+               date: DateFormatter.localizedString(from: Date(), dateStyle: .short, timeStyle: .none), // Current date
+               question: questionText,
+               solution: "Solution will be added later", // Placeholder solution
+               solutionImage: nil // Optionally add image for the solution
+           )
+          
+          // Pass the new doubt to the delegate (DoubtsListTableViewController)
+          delegate?.didAddDoubt(newDoubt)
 
-        private func updateLessons(for subject: String) {
-            currentLessons = lessonsMapping[subject] ?? []
-            var lessonActions: [UIAction] = []
-            for lesson in currentLessons {
-                let action = UIAction(title: lesson) { [weak self] _ in
-                    self?.lessonLabel.text = lesson
-                }
-                lessonActions.append(action)
-            }
-            let lessonMenu = UIMenu(title: "Choose Lesson", children: lessonActions)
-            lessonButton.menu = lessonMenu
-            lessonButton.showsMenuAsPrimaryAction = true
-        }
-
-        // MARK: - Done Button Action
-        @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
-            guard let subject = subjectLabel.text,
-                  let lesson = lessonLabel.text else { return }
-
-            let newDoubt = DoubtsListTableViewController.Subject(
-                image: "default_image", 
-                subjectName: subject,
-                lessonName: lesson,
-                status: "Pending",
-                date: DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .none),
-                question: "Sample question text",
-                solution: "",
-                solutionImage: nil
-            )
-
-            delegate?.didAddDoubt(newDoubt)
-
-            let alertController = UIAlertController(
-                title: "Request Sent",
-                message: "Your request has been sent to the tutor successfully.",
-                preferredStyle: .alert
-            )
-            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-                self.navigationController?.popViewController(animated: true)
-            }))
-            self.present(alertController, animated: true, completion: nil)
-            print("New doubt added: \(newDoubt)")
-
-        }
+          // Optionally, close this view controller
+          navigationController?.popViewController(animated: true)
+      }
+        // Continue with doubt submission...
     }
 
 
+// MARK: - UICollectionView DataSource and Delegate
+extension DoubtTableViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return selectedImages.count
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCollectionViewCell
+        cell.imageView.image = selectedImages[indexPath.item]
+        cell.removeButton.tag = indexPath.item
+        cell.removeButton.addTarget(self, action: #selector(removeImage(_:)), for: .touchUpInside)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let fullScreenVC = FullScreenImageViewController()
+        fullScreenVC.image = selectedImages[indexPath.item]
+        present(fullScreenVC, animated: true, completion: nil)
+    }
+
+    @objc private func removeImage(_ sender: UIButton) {
+        selectedImages.remove(at: sender.tag)
+        collectionView.reloadData()
+    }
+
+    // Layout for the collection view
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+}
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var removeButton: UIButton!
+    
+}
+
+class FullScreenImageViewController: UIViewController {
+    var image: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let imageView = UIImageView(frame: self.view.bounds)
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = image
+        self.view.addSubview(imageView)
+        
+        let closeButton = UIButton(frame: CGRect(x: 20, y: 50, width: 50, height: 50))
+        closeButton.setTitle("Close", for: .normal)
+        closeButton.addTarget(self, action: #selector(closeTapped), for: .touchUpInside)
+        self.view.addSubview(closeButton)
+    }
+    
+    @objc private func closeTapped() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
