a1fb03f8dc7fb8f93c624eaab9d5a953bc567b38
Praveen Shrivastav <praveenshrivastav553@gmail.com>
2025-02-10 11:21:48 +0530

    Added: Data Controllers in ConnectHub

--- LearnSathi/Controller/Connect/ConnectViewController.swift
+++ LearnSathi/Controller/Connect/ConnectViewController.swift
@@ -4,53 +4,49 @@
 //
 //  Created by Batch - 1 on 07/01/25.
 //
-
+ 
 import UIKit
 
 class ConnectViewController: UIViewController {
-
     var selectedIndexPath: IndexPath?
     
+    @IBOutlet weak var tutorListCollectionView: UICollectionView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerConnectCells()
+    }
+    
+    private func registerConnectCells() {
+        tutorListCollectionView?.register(UINib(nibName: TutorListCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: TutorListCollectionViewCell.identifier)
+        tutorListCollectionView?.dataSource = self
+        tutorListCollectionView?.delegate = self
+    }
+}
+
+extension ConnectViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return ConnectDataController.shared.getTutors().count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TutorListCollectionViewCell.identifier, for: indexPath) as! TutorListCollectionViewCell
+        let tutors = ConnectDataController.shared.getTutors()
+        cell.setup(tutor: tutors[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedIndexPath = indexPath
+        let tutors = ConnectDataController.shared.getTutors()
+        let selectedTutor = tutors[indexPath.row]
+        let storyboard = UIStoryboard(name: "CustomOptions", bundle: nil)
         
-        
-        @IBOutlet weak var collectionView: UICollectionView!
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            registerConnectCells()
-        }
-        
-        private func registerConnectCells() {
-            collectionView?.register(UINib(nibName: TutorListCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: TutorListCollectionViewCell.identifier)
-            collectionView?.dataSource = self
-            collectionView?.delegate = self
+        if let secondaryVc = storyboard.instantiateViewController(withIdentifier: "CustomOptions") as? CustomOptionsTableViewController {
+            secondaryVc.tutorProfileImage = UIImage(named: selectedTutor.tutorProfileImage)
+            secondaryVc.tutorName = selectedTutor.tutorName
+            secondaryVc.subjectName = selectedTutor.subject
+            navigationController?.pushViewController(secondaryVc, animated: true)
         }
     }
-
-
-        extension ConnectViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return tutors.count
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TutorListCollectionViewCell.identifier, for: indexPath) as! TutorListCollectionViewCell
-            cell.setup(tutor: tutors[indexPath.row])
-            return cell
-        }
-            
-            
-        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-            selectedIndexPath = indexPath
-            let selectedTutor = tutors[indexPath.row]
-            let storyboard = UIStoryboard(name: "CustomOptions", bundle: nil)
-            if let secondaryVc = storyboard.instantiateViewController(withIdentifier: "CustomOptions") as? CustomOptionsTableViewController {
-                secondaryVc.tutorProfileImage = UIImage(named: selectedTutor.tutorProfileImage)
-                secondaryVc.tutorName = selectedTutor.tutorName
-                secondaryVc.subjectName = selectedTutor.subject
-                            navigationController?.pushViewController(secondaryVc, animated: true)
-            }
-
-        }
 }
--- LearnSathi/Views/Connect/DoubtSolution/SolutionTableViewController.swift
+++ LearnSathi/Views/Connect/DoubtSolution/SolutionTableViewController.swift
@@ -6,80 +6,92 @@
 //
 
 import UIKit
+import QuickLook
 
-class SolutionTableViewController: UITableViewController {
-
+class SolutionTableViewController: UITableViewController, QLPreviewControllerDataSource {
+    
     var doubt: Doubts?
-    
-    
-    @IBOutlet weak var subjectLabel: UILabel!
-    
-    @IBOutlet weak var lessonLabel: UILabel!
-    
-    @IBOutlet weak var questionLabel: UILabel!
-    
-    @IBOutlet weak var solutionLabel: UILabel!
-    
-    @IBOutlet weak var solutionImageCollectionView: UICollectionView!
-    
-    
-    // MARK: - Populate Data
-        private func populateData() {
-            guard let doubt = doubt else { return }
-            subjectLabel.text = doubt.subjectName
-            lessonLabel.text = doubt.lessonName
-            questionLabel.text = doubt.question
-            solutionLabel.text = doubt.solution
-            print("Doubt passed successfully: \(doubt)")
-        }
+    private var selectedImageURL: URL?
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            setupCollectionView()
-            populateData()
-        }
-        
-        // MARK: - Setup CollectionView
-        private func setupCollectionView() {
-            solutionImageCollectionView.dataSource = self
-            solutionImageCollectionView.delegate = self
-            let nib = UINib(nibName: "SolutionImageCollectionViewCell", bundle: nil)
-            solutionImageCollectionView.register(nib, forCellWithReuseIdentifier: "SolutionImageCell")
-        }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let image = doubt?.solutionImages[indexPath.item] else { return }
-        
-        let fullScreenVC = FullScreenImageViewController()
-        fullScreenVC.image = image
-        fullScreenVC.modalPresentationStyle = .overFullScreen
-        fullScreenVC.modalTransitionStyle = .crossDissolve
-        present(fullScreenVC, animated: true, completion: nil)
+    @IBOutlet weak var subjectLabel: UILabel!
+    @IBOutlet weak var lessonLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var solutionLabel: UILabel!
+    @IBOutlet weak var solutionImageCollectionView: UICollectionView!
+
+    // MARK: - Populate Data
+    private func populateData() {
+        guard let doubt = doubt else { return }
+        subjectLabel.text = doubt.subjectName
+        lessonLabel.text = doubt.lessonName
+        questionLabel.text = doubt.question
+        solutionLabel.text = doubt.solution
+        print("Doubt passed successfully: \(doubt)")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+        populateData()
+    }
+
+    // MARK: - Setup CollectionView
+    private func setupCollectionView() {
+        solutionImageCollectionView.dataSource = self
+        solutionImageCollectionView.delegate = self
+        let nib = UINib(nibName: "SolutionImageCollectionViewCell", bundle: nil)
+        solutionImageCollectionView.register(nib, forCellWithReuseIdentifier: "SolutionImageCell")
     }
 
     @IBAction func cancelButtonTapped(_ sender: Any) {
         dismiss(animated: true)
     }
+
+    // MARK: - QLPreviewController DataSource
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return selectedImageURL != nil ? 1 : 0
+    }
+
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        return selectedImageURL! as QLPreviewItem
+    }
+
+    // MARK: - Open Image in Fullscreen using QuickLook
+    private func openImageInFullScreen(_ image: UIImage) {
+        let tempURL = FileManager.default.temporaryDirectory.appendingPathComponent("tempImage.jpg")
+        if let imageData = image.jpegData(compressionQuality: 1.0) {
+            try? imageData.write(to: tempURL)
+            selectedImageURL = tempURL
+
+            let previewController = QLPreviewController()
+            previewController.dataSource = self
+            present(previewController, animated: true)
+        }
+    }
 }
 
-    // MARK: - UICollectionView DataSource & Delegate
-    extension SolutionTableViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-        
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return doubt?.solutionImages.count ?? 0
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SolutionImageCell", for: indexPath) as! SolutionImageCollectionViewCell
-            if let image = doubt?.solutionImages[indexPath.item] {
-                cell.solutionImageView.image = image
-            }
-            return cell
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-            return CGSize(width: 100, height: 100)
-        }
-
+// MARK: - UICollectionView DataSource & Delegate
+extension SolutionTableViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return doubt?.solutionImages.count ?? 0
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SolutionImageCell", for: indexPath) as! SolutionImageCollectionViewCell
+        if let image = doubt?.solutionImages[indexPath.item] {
+            cell.solutionImageView.image = image
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let image = doubt?.solutionImages[indexPath.item] else { return }
+        openImageInFullScreen(image)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+}
 
