955e62400a18c1c9dd8526a5c07db0376f637db7
Md. Akhlak <mohammadakhlak121@gmail.com>
2025-02-12 16:46:05 +0530

    modified user profile and added functionality

--- LearnSathi/Data Model/User.swift
+++ LearnSathi/Data Model/User.swift
@@ -14,4 +14,12 @@ struct UserAuth {
     var standard: String
     var email: String
     var number: String
+    var profileImage: String?
+
 }
+
+var users : [UserAuth] = [
+    
+    UserAuth(id: UUID(), firstName: "Ayush", lastName: "Singh", standard: "7", email: "axcy121@gmail.com", number: "203232302039", profileImage: "profileImage"),
+    UserAuth(id: UUID(), firstName: "Abu", lastName: "Shahma", standard: "8", email: "fuzail211@gmail.com", number: "203232302039", profileImage: "user2"),
+]
---
+++ LearnSathi/Views/Home/SwitchAccountTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SwitchAccountTableViewCell.swift
+//  LearnSathi
+//
+//  Created by Akhlak iSDP on 11/02/25.
+//
+
+import UIKit
+
+class SwitchAccountTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
